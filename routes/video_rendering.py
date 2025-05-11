67fb1ce2b7681c09bf5a1661b533ffc4c8d4b7e4
yashksaini-coder <ys3853428@gmail.com>
2025-05-12 00:08:07 +0530

    Add python executable

--- routes/video_rendering.py
+++ routes/video_rendering.py
@@ -137,6 +137,8 @@ config.frame_width = {frame_width}
         video_file_path = None
         try:
             command_list = [
+                "python",
+                "-m",
                 "manim",
                 file_path,
                 file_class,
